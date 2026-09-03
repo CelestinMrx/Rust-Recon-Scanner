14cf2cdcddf18b7e1c90d58a6a9843d22207396b
CelestinMrx <celestin.mireux@gmail.com>
2026-09-03 19:00:37 +0200

    implémentation de l'envoi de requête HTTP pour le banner grabbing

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,20 @@ use clap::Parser;
 
 use scanner::ScanResult;
 use scanner::scan_ports;
-use scanner::grab_banner;
+//use scanner::grab_banner;
 
 use ascii_art::SWORD;
 
 #[tokio::main]
 async fn main() {
     let args : Cli = Cli::parse();
-    /*let concurrency: usize = 200;
+    let concurrency: usize = 200;
     let scan: Vec<ScanResult> = scan_ports(args.ip, args.start_port, args.end_port, args.timeout_ms, concurrency).await;   
     println!("{}", SWORD);
     println!("Scan report for {}", args.ip);
     println!("PORT   SERVICE");
     for port in &scan {
-        println!("{}   Unknown", port.port);
-    } */
-
-    let banner = grab_banner(args.ip, args.start_port, args.timeout_ms).await;
-
-    println!("{:?}", banner);
+        println!("{}   {:?}", port.port, port.banner);
+        println!("");
+    } 
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -10,6 +10,40 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 pub struct ScanResult {
     pub port: u16,
     pub is_open: bool,
+    pub banner: Option<String>
+}
+
+pub async fn read_with_timeout(stream: &mut TcpStream, timeout_ms: u64) -> Option<String> {
+    let mut bytes: Vec<u8> = vec![0; 1024];
+    let resp = timeout(
+                    Duration::from_millis(timeout_ms),
+                    stream.read(&mut bytes)).await;
+    match resp {
+            Ok(Ok(n)) => Some(String::from_utf8_lossy(&bytes[..n]).to_string()),
+            Ok(Err(_)) => None,
+            Err(_) => None
+    }
+}
+
+pub async fn grab_banner(ip: Ipv4Addr, port: u16, timeout_ms: u64) -> Option<String> {
+    let socket: SocketAddr = SocketAddr::new(IpAddr::V4(ip), port);
+
+    let res: Result<Result<TcpStream, std::io::Error>, tokio::time::error::Elapsed> = timeout(
+        Duration::from_millis(timeout_ms),
+        TcpStream::connect(socket),
+    )
+    .await;
+
+    match res {
+        Ok(Ok(mut stream)) => {let awnser = read_with_timeout(&mut stream, timeout_ms).await;
+                                            match awnser {
+                                                None => {stream.write_all(b"GET / HTTP/1.0\r\n\r\n").await.ok();
+                                                        read_with_timeout(&mut stream, timeout_ms).await},
+                                                _ => awnser
+                                            }
+                                        },
+        _ => None,
+    }
 }
 
 pub async fn scan_port(ip: Ipv4Addr, port: u16, timeout_ms: u64) -> ScanResult {
@@ -25,39 +59,18 @@ pub async fn scan_port(ip: Ipv4Addr, port: u16, timeout_ms: u64) -> ScanResult {
         Ok(Ok(_)) => ScanResult {
             port,
             is_open: true,
+            banner: grab_banner(ip, port, timeout_ms).await
         },
         Ok(Err(_)) => ScanResult {
             port,
             is_open: false,
+            banner: None
         },
         Err(_) => ScanResult {
             port,
             is_open: false,
+            banner: None
         },
-    }
-}
-
-pub async fn grab_banner(ip: Ipv4Addr, port: u16, timeout_ms: u64) -> Option<String> {
-    let socket: SocketAddr = SocketAddr::new(IpAddr::V4(ip), port);
-
-    let res: Result<Result<TcpStream, std::io::Error>, tokio::time::error::Elapsed> = timeout(
-        Duration::from_millis(timeout_ms),
-        TcpStream::connect(socket),
-    )
-    .await;
-
-    let mut bytes: Vec<u8> = vec![0; 1024];
-
-    match res {
-        Ok(Ok(mut stream)) => {let resp = timeout(
-                                    Duration::from_millis(timeout_ms),
-                                    stream.read(&mut bytes)).await;
-                                    match resp {
-                                        Ok(Ok(n)) => Some(String::from_utf8_lossy(&bytes[..n]).to_string()),
-                                        Ok(Err(_)) => None,
-                                        Err(_) => None
-                                    }},
-        _ => None
     }
 }
 
